a5f819c098a413d198d3fdd8973ca883906659df
Jess <jessdev83@gmail.com>
2019-03-02 10:35:09 -0500

    Top Nav Skeleton
    basic component built, no functionality yet. very basic styling to place elements in the correct spot done. still needs a logo image. icons folder created.

--- client/src/Components/shared/TopNavBar/container.js
+++ client/src/Components/shared/TopNavBar/container.js
@@ -1,8 +1,26 @@
 import React from "react";
+import "./topnav.css";
 
 class TopNav extends React.Component {
   render() {
-    return <h1>This is the top nav bar</h1>;
+    return (
+      <div className="top-nav">
+        <div className="home-menu">
+          <img src={require("./icons/menu.svg")} alt="" />
+          <p>Home</p>
+        </div>
+        <div className="logo">
+          <p>Logo Goes Here</p>
+        </div>
+        <div className="notify-help">
+          <img
+            src={require("./icons/notification-bell.svg")}
+            alt="Notifications"
+          />
+          <img src={require("./icons/help-button.svg")} alt="Help" />
+        </div>
+      </div>
+    );
   }
 }
 
