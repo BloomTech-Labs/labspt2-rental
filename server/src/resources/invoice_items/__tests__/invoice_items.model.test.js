1f28abc919b70288561bb778be69b0b9c8d5d7c8
mglittleton <michaelglittleton@gmail.com>
2019-03-06 00:48:43 -0500

    adds tests to the invoice_items API

---
+++ server/src/resources/invoice_items/__tests__/invoice_items.model.test.js
@@ -0,0 +1,60 @@
+import { InvoiceItem } from '../invoice_items.model';
+import mongoose from 'mongoose';
+
+describe('Invoice items model', () => {
+  describe('schema', () => {
+    test('name', () => {
+      const { name } = InvoiceItem.schema.obj;
+      expect(name).toEqual({
+        type: String,
+        required: [true, 'Name is a required field'],
+        maxlength: 100
+      });
+    });
+
+    test('description', () => {
+      const { description } = InvoiceItem.schema.obj;
+      expect(description).toEqual({
+        type: String,
+        maxlength: 500
+      });
+    });
+
+    test('price', () => {
+      const { price } = InvoiceItem.schema.obj;
+      expect(price).toEqual({
+        type: Number,
+        required: [true, 'A price must be assigned'],
+        min: 0
+      });
+    });
+
+    test('owner', () => {
+      const { owner } = InvoiceItem.schema.obj;
+      expect(owner).toEqual([
+        {
+          type: mongoose.Types.ObjectId,
+          ref: 'user'
+        }
+      ]);
+    });
+
+    test('defaultItem', () => {
+      const { defaultItem } = InvoiceItem.schema.obj;
+      expect(defaultItem).toEqual({ 
+        type: Boolean,
+        default: false,
+        required: [true, 'Default distinction is required'] 
+      });
+    });
+
+    test('lastUsed', () => {
+      const { lastUsed } = InvoiceItem.schema.obj;
+      expect(lastUsed).toEqual({ 
+        type: Date,
+        default: Date.now(),
+        required: [true, 'Last used date is a required field'] 
+      });
+    });
+  });
+});
