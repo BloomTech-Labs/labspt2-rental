cef2c7e9afa50136702e71ded8919085f4d76999
mglittleton <michaelglittleton@gmail.com>
2019-03-05 23:38:23 -0500

    added invoice items API
    API includes: name, description, price, owner, defaultItem (Boolean), lastUsed

---
+++ server/src/resources/invoice_items/invoice_items.router.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import controllers from './invoice_items.controllers';
+
+const { Router } = express;
+const router = Router;
+
+router
+  .route('/')
+  .get(controllers.getMany)
+  .post(controllers.createOne);
+
+router
+  .route('/:id')
+  .get(controllers.getOne)
+  .put(controllers.updateOne)
+  .delete(controllers.deleteOne);
