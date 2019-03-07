b79577e0921592f7b028ec92f1a118f0bf32d626
BryceEvans <evans.bryce@gmail.com>
2019-03-06 20:25:02 -0700

    toml file added in prepeartion for deployment.

---
+++ client/src/Components/Dashboard/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Dashboard extends Component {
+
+  render() {
+    return (
+      <div>
+        This is the Dashboard.
+      </div>
+    )
+  }
+}
