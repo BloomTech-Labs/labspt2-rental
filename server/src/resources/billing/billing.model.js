5b105979f9f4a6d6bacb8c1339d08650ccccdbcd
Julie Jonak <juliejonak10@gmail.com>
2019-03-07 18:55:49 -0600

    Filled in billingSchema

--- server/src/resources/billing/billing.model.js
+++ server/src/resources/billing/billing.model.js
@@ -4,7 +4,19 @@ const { Schema } = mongoose;
 
 const billingSchema = new Schema(
   {
-    
+    name: {
+        type: String,
+        required: true,
+        enum: ['Free', 'Midlevel', 'Enterprise']
+    },
+    perPropertyPrice: {
+        type: Number,
+        required: true
+    },
+    perTransactionFee: {
+        type: Number,
+        required: true
+    }
   }, 
   { timestamps: true }
 );
