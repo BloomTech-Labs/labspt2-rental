65c5614f79bb191f202b43c9f9f09857c45dc59e
Jess Harrison <jessdev83@gmail.com>
2019-03-07 22:46:35 -0500

    actions and reducer completed

---
+++ client/src/Components/UserList/actions.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+export const LOADING = "LOADING";
+export const FOUND_USERS = "FOUND_USERS";
+export const ERROR = "ERROR";
+
+export const fetchUsers = () => {
+  return dispatch => {
+    dispatch({
+      type: LOADING
+    });
+    axios
+      .get("http://138.197.202.158/api/users/all")
+      .then(response => {
+        dispatch({
+          type: FOUND_USERS,
+          payload: response.data
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        dispatch({
+          type: ERROR,
+          payload: "Could not fetch users."
+        });
+      });
+  };
+};
--- client/src/Components/UserList/reducer.js
+++ client/src/Components/UserList/reducer.js
@@ -1,7 +1,19 @@
-const initialState = null;
+import { LOADING, FOUND_USERS, ERROR } from "./actions";
+
+const initialState = {
+  users: [],
+  loading: false,
+  error: ""
+};
 
 const userListReducer = (state = initialState, action) => {
   switch (action.type) {
+    case LOADING:
+      return { ...state, loading: true };
+    case FOUND_USERS:
+      return { ...state, users: action.payload, loading: false };
+    case ERROR:
+      return { ...state, error: action.payload, loading: false };
     default:
       return state;
   }
