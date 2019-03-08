de9b45f4834bfbda2499945736176f961aba0fdb
Michael Littleton <michaelglittleton@gmail.com>
2019-03-07 22:47:27 -0500

    adds discount API
    discount API includes name, description, discountPercentage, owner, lastUsed

---
+++ server/src/resources/discount/__tests__/discount.model.test.js
@@ -0,0 +1,52 @@
+import { Discount } from '../discount.model';
+import mongoose from 'mongoose';
+
+describe('discount model', () => {
+  describe('schema', () => {
+    test('name', () => {
+      const { name } = Discount.schema.obj;
+      expect(name).toEqual({
+        type: String,
+        required: [true, 'Name is a required field'],
+        maxlength: 100
+      });
+    });
+
+    test('description', () => {
+      const { description } = Discount.schema.obj;
+      expect(description).toEqual({
+        type: String,
+        maxlength: 500
+      });
+    });
+
+    test('discountPercentage', () => {
+      const { discountPercentage } = Discount.schema.obj;
+      expect(discountPercentage).toEqual({
+        type: Number,
+        required: [true, 'A discount percentage must be assigned'],
+        min: 0,
+        max: 1
+      });
+    });
+
+    test('owner', () => {
+      const { owner } = Discount.schema.obj;
+      expect(owner).toEqual([
+        {
+          type: mongoose.Types.ObjectId,
+          ref: 'user'
+        }
+      ]);
+    });
+
+    test('lastUsed', () => {
+      const { lastUsed } = Discount.schema.obj;
+      expect(lastUsed).toEqual({ 
+        type: Date,
+        default: Date.now(),
+        required: [true, 'Last used date is a required field'] 
+      });
+    });
+  });
+});
---
+++ server/src/resources/discount/discount.model.js
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+const discountSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Discount requires a name'],
+      maxlength: 100
+    },
+    description: {
+      type: String,
+      maxlength: 500
+    },
+    discountPercentage: {
+      type: Number,
+      required: [true, 'A discount amount is required'],
+      min: 0,
+      max: 1
+    },
+    owner: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: 'user'
+      }
+    ],
+    lastUsed: {
+      type: Date,
+      default: Date.now(),
+      required: [true, 'Last used date is a required field']
+    }
+  },
+  { timestamps: true }
+);
+
+export const Discount = mongoose.model('discount', discountSchema);
