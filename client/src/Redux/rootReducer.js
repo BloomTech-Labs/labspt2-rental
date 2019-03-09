c6bcd79ba6eaa800b8fb33f8699c49c8739aac09
Jess Harrison <jessdev83@gmail.com>
2019-03-08 22:18:17 -0500

    added userlist to root reducer

--- client/src/Redux/rootReducer.js
+++ client/src/Redux/rootReducer.js
@@ -1,6 +1,7 @@
 import { combineReducers } from "redux";
 import { propertiesReducer } from "../Components/Properties/PropList/reducers";
 import registrationReducer from "../Components/Registration/reducer";
+import { userListReducer } from "../Components/UserList/reducer";
 
 export default combineReducers({
   properties: () => null,
