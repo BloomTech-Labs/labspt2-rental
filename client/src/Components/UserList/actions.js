2eac23feeb613e5111de015d1da8b96c59091bdc
Jess Harrison <jessdev83@gmail.com>
2019-03-08 21:28:57 -0500

    Update actions.js

--- client/src/Components/UserList/actions.js
+++ client/src/Components/UserList/actions.js
@@ -1,26 +1,26 @@
 import axios from "axios";
 
-export const LOADING = "LOADING";
-export const FOUND_USERS = "FOUND_USERS";
-export const ERROR = "ERROR";
+export const FETCH_USERS_ATTEMPT = "FETCH_USERS_ATTEMPT";
+export const FETCH_USERS_SUCCESS = "FETCH_USERS_SUCCESS";
+export const FETCH_USERS_FAILURE = "FETCH_USERS_FAILURE";
 
 export const fetchUsers = () => {
   return dispatch => {
     dispatch({
-      type: LOADING
+      type: FETCH_USERS_ATTEMPT
     });
     axios
       .get("http://138.197.202.158/api/users")
       .then(response => {
         dispatch({
-          type: FOUND_USERS,
+          type: FETCH_USERS_SUCCESS,
           payload: response.data
         });
       })
       .catch(err => {
         console.log(err);
         dispatch({
-          type: ERROR,
+          type: FETCH_USERS_FAILURE,
           payload: "Could not fetch users."
         });
       });
