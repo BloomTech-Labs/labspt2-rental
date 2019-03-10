74c023037ae29aa6ab7ddb83eef094316b6575dc
Michael Littleton <michaelglittleton@gmail.com>
2019-03-10 14:54:13 -0400

    Adds employee API
    Employee API includes name, baseAddress, user, createdBy, employer, and permission statuses for tasks, properties, and checkouts

---
+++ server/src/resources/employee/__tests__/employee.controllers.test.js
@@ -0,0 +1,18 @@
+import { controllers } from '../employee.controllers';
+import { isFunction } from 'lodash';
+
+describe('employees controllers', () => {
+  test('has crud controllers', () => {
+    const crudMethods = [
+      'getOne',
+      'getMany',
+      'createOne',
+      'removeOne',
+      'updateOne'
+    ];
+
+    crudMethods.forEach(name =>
+      expect(isFunction(controllers[name])).toBe(true)
+    );
+  });
+});
