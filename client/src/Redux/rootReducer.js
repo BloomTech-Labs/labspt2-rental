e0d4716ac599800851d1e83c5d3cb03467a991d0
Jess Harrison <jessdev83@gmail.com>
2019-03-10 10:51:30 -0400

    Export error
    found error on netlify deploy regarding export of user reducer. corrected error in rootReducer file, app is rendering as it should again.

--- client/src/Redux/rootReducer.js
+++ client/src/Redux/rootReducer.js
@@ -1,7 +1,7 @@
 import { combineReducers } from "redux";
-import { propertiesReducer } from "../Components/Properties/PropList/reducers";
+import propertiesReducer from "../Components/Properties/PropList/reducers";
 import registrationReducer from "../Components/Registration/reducer";
-import { userListReducer } from "../Components/UserList/reducer";
+import userListReducer from "../Components/UserList/reducer";
 
 export default combineReducers({
   properties: () => null,
