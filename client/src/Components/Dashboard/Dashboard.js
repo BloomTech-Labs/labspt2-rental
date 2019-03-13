9841cb5f348219b1b76b1f6d82185c1426428ec1
Kyle Baker <tokylebaker@gmail.com>
2019-03-12 17:09:19 -0700

    sidebar

--- client/src/Components/Dashboard/Dashboard.js
+++ client/src/Components/Dashboard/Dashboard.js
@@ -1,26 +1,26 @@
 import React, { Component } from 'react'
 import { Route, Link, withRouter } from 'react-router-dom'
-import { Segment, Menu } from 'semantic-ui-react'
+import { Segment, Menu, Sidebar, Icon, Header, Image } from 'semantic-ui-react'
 import { FlexRow, Container } from 'custom-components'
 import { Reservations } from '../Reservations'
 
 class Dashboard extends Component {
   constructor (props) {
-    super(props);
+    super(props)
 
     let currentRoute = props.location.pathname.slice(11, props.location.pathname.length) // Grab the name of the current route
-    currentRoute = currentRoute.charAt(0).toUpperCase() + currentRoute.slice(1); // Capitalize the first letter
+    currentRoute = currentRoute.charAt(0).toUpperCase() + currentRoute.slice(1) // Capitalize the first letter
 
     this.state = {
       active: currentRoute // Set active menu item to current route
     }
 
     this.links = [
-      { url: `/dashboard/reservations`, name: 'Reservations' },
-      { url: `/dashboard/checkout`, name: 'Checkout' },
-      { url: `/dashboard/properties`, name: 'Properties' },
-      { url: `/dashboard/tasks`, name: 'Tasks' },
-      { url: `/dashboard/settings`, name: 'Settings' },
+      { url: `/dashboard/reservations`, name: 'Reservations', icon: 'book' },
+      { url: `/dashboard/checkout`, name: 'Checkout', icon: 'cart' },
+      { url: `/dashboard/properties`, name: 'Properties', icon: 'home' },
+      { url: `/dashboard/tasks`, name: 'Tasks', icon: 'tasks' },
+      { url: `/dashboard/settings`, name: 'Settings', icon: 'setting' },
     ]
   }
 
@@ -32,22 +32,22 @@ class Dashboard extends Component {
     const { active } = this.state
 
     return (
-      <Container alignCenter padding>
-        <Segment>
-          <FlexRow>
-            <div style={{ width: '200px' }}>
-              <Menu color="green" fluid vertical tabular style={{ borderColor: "#35ba45"}}> {/*Border color can be set in the theme if we like it*/}
-                {this.links.map((link, ind) => (
-                  <Link key={ind} to={link.url} onClick={this.handleClick}>
-                    <Menu.Item name={link.name} active={active === link.name}/>
-                  </Link>
-                ))}
-              </Menu>
-            </div>
+      <Container>
+        <Sidebar.Pushable style={{width: "100%"}}>
+          <Sidebar as={Menu} animation='overlay' icon='labeled' inverted vertical visible width='thin'>
+            {this.links.map((link, ind) => (
+              <Link key={ind} to={link.url} onClick={this.handleClick}>
+                <Menu.Item as='a'>
+                  <Icon name={link.icon}/>
+                  {link.name}
+                </Menu.Item>
+              </Link>
+            ))}
 
+          </Sidebar>
 
-            <FlexRow grow="1">
-              <Segment className="space-left-20" style={{ width: '100%' }}>
+          <Sidebar.Pusher as={Segment} style={{marginLeft: "140px"}}>
+              <Segment className="space-left-20" >
                 <Route path="/dashboard/reservations" render={() => <Reservations/>}/>
                 {/*TODO work on these pages*/}
                 {/*<Route path="/dashboard/checkout" render={() => <Checkout/>}/>*/}
@@ -55,9 +55,8 @@ class Dashboard extends Component {
                 {/*<Route path="/dashboard/tasks" render={() => <Tasks/>}/>*/}
                 {/*<Route path="/dashboard/settings" render={() => <Settings/>}/>*/}
               </Segment>
-            </FlexRow>
-          </FlexRow>
-        </Segment>
+          </Sidebar.Pusher>
+        </Sidebar.Pushable>
       </Container>
     )
   }
