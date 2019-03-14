4b352fb574210e80b591bfc203c9a1eff315ce12
Jess Harrison <jessdev83@gmail.com>
2019-03-13 22:28:43 -0400

    Moved methods and state out of shared component

--- client/src/Components/shared/DatePicker.js
+++ client/src/Components/shared/DatePicker.js
@@ -1,24 +1,14 @@
-import React, { Component } from "react";
+import React from "react";
 import Calendar from "react-calendar";
 import FlexRow from "../../styled-components/index";
 
-class DatePicker extends Component {
-  state = {
-    startDate: new Date(),
-    endDate: new Date()
-  };
-  //need to come back and make this DRY.
-  startDate = date => this.setState({ startDate: date });
-  endDate = date => this.setState({ endDate: date });
-
-  render() {
-    return (
-      <FlexRow>
-        <Calendar onChange={this.startDate} value={this.state.startDate} />
-        <Calendar onChange={this.endDate} value={this.state.startDate} />
-      </FlexRow>
-    );
-  }
-}
+const DatePicker = props => {
+  return (
+    <FlexRow>
+      <Calendar onChange={props.start} value={props.startDate} />
+      <Calendar onChange={props.end} value={props.endDate} />
+    </FlexRow>
+  );
+};
 
 export default DatePicker;
