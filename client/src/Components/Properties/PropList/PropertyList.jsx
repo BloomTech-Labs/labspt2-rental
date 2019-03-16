2851998308acd428de00a68cebab8d7f165151ab
Jess Harrison <jessdev83@gmail.com>
2019-03-15 23:39:53 -0400

    Date picker complete

--- client/src/Components/Properties/PropList/PropertyList.jsx
+++ client/src/Components/Properties/PropList/PropertyList.jsx
@@ -1,28 +1,10 @@
 import React, { Component } from "react";
 //import card component when finished
-import DatePicker from "../../shared/DatePicker";
+import DatePicker from "../../shared/DatePicker/DatePicker";
 
 class PropertyList extends Component {
-  state = {
-    startDate: null,
-    endDate: null,
-    focusedInput: null
-  };
-
-  onDatesChange = ({ startDate, endDate }) => this.setState({ startDate, endDate })
-  
-  onFocusChange = focusedInput => this.setState({ focusedInput })
-
   render() {
-    return (
-      <DatePicker
-        start={this.startDate}
-        end={this.endDate}
-        onDatesChange={this.onDatesChange}
-        focusedInput={this.state.focusedInput}
-        onFocusChange={this.onFocusChange}
-      />
-    );
+    return <DatePicker />;
   }
 }
 
