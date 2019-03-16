af88766616e886dec2f3a03efefb2b4b9b301dce
Jess Harrison <jessdev83@gmail.com>
2019-03-15 23:45:37 -0400

    Fixed merge conflicts

--- client/src/Components/Dashboard/Dashboard.js
+++ client/src/Components/Dashboard/Dashboard.js
@@ -5,7 +5,7 @@ import { FlexRow, Container } from "custom-components";
 import { Reservations } from "../Reservations";
 import { EmployeeList } from "../Employees";
 import { PropertyList } from "../Properties/PropList";
-
+import { Checkout } from "../Reservations";
 
 class Dashboard extends Component {
   constructor(props) {
@@ -60,7 +60,6 @@ class Dashboard extends Component {
             ))}
           </Sidebar>
 
-<<<<<<< HEAD
           <Sidebar.Pusher as={Segment} style={{ marginLeft: "140px" }}>
             <Segment className="space-left-20">
               <Route
@@ -72,7 +71,7 @@ class Dashboard extends Component {
                 render={() => <EmployeeList />}
               />
               {/*TODO work on these pages*/}
-              {/*<Route path="/dashboard/checkout" render={() => <Checkout/>}/>*/}
+              <Route path="/dashboard/checkout" render={() => <Checkout />} />
               <Route
                 path="/dashboard/properties"
                 render={() => <PropertyList />}
@@ -80,18 +79,6 @@ class Dashboard extends Component {
               {/*<Route path="/dashboard/tasks" render={() => <Tasks/>}/>*/}
               {/*<Route path="/dashboard/settings" render={() => <Settings/>}/>*/}
             </Segment>
-=======
-          <Sidebar.Pusher as={Segment} style={{marginLeft: "140px"}}>
-              <Segment className="space-left-20" >
-                <Route path="/dashboard/reservations" render={() => <Reservations/>}/>
-                <Route path="/dashboard/employees" render={props => <EmployeeList {...props}/>}/>
-                {/*TODO work on these pages*/}
-                <Route path="/dashboard/checkout" render={() => <Checkout/>}/>
-                {/*<Route path="/dashboard/properties" render={() => <Properties/>}/>*/}
-                {/*<Route path="/dashboard/tasks" render={() => <Tasks/>}/>*/}
-                {/*<Route path="/dashboard/settings" render={() => <Settings/>}/>*/}
-              </Segment>
->>>>>>> 0ec7d1d9881e865f9dbac6a8d0d8dad616dba1d9
           </Sidebar.Pusher>
         </Sidebar.Pushable>
       </Container>
