4bafca01e1e550f8960f277fc705b6ef0baee2cf
Julie Jonak <juliejonak10@gmail.com>
2019-03-15 21:58:20 -0500

    Setting up Settings: - actions.js: Exporting actions - actionCreator.js: Creating updateUser functionality

---
+++ client/src/Components/Settings/actionCreator.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import * as actions from './actions';
+
+// updateUser
+// CC info and next billing will come from Stripe
+// hard code billing plan IDs into update modal.
+
+// keep Store updated with user info. If it changes, update
+// use the billing plan ID to query billing table and send that to Stripe
+
+export const updateUser = (user) => {
+    return async dispatch => {
+        dispatch({type: actions.UPDATE_USER_STARTED})
+        try {
+            const updatedUser = await axios.put(`http://`, user)
+            console.log(updatedUser)
+            dispatch({
+                type: actions.UPDATE_USER_SUCCESS,
+                payload: updatedUser
+            })   
+        } catch (err) {
+            console.error(err)
+            dispatch({type: actions.UPDATE_USER_ERROR, payload: err})
+        }
+    }
+}
