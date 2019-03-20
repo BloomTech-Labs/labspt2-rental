b813fd6b5c5080d5aa39b159decfc3a6f3a46055
Julie Jonak <juliejonak10@gmail.com>
2019-03-20 14:16:24 -0500

    Edited client side stripe

--- client/src/Components/Settings/actionCreator.js
+++ client/src/Components/Settings/actionCreator.js
@@ -12,7 +12,7 @@ export const updateUser = (user) => {
     return async dispatch => {
         dispatch({type: actions.UPDATE_USER_STARTED})
         try {
-            const updatedUser = await axios.put(`http://`, user)
+            const updatedUser = await axios.put(`${config.apiUrl}/api/endpoint`, user)
             console.log(updatedUser)
             dispatch({
                 type: actions.UPDATE_USER_SUCCESS,
