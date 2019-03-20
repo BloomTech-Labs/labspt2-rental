a2efe611ba0f4e89d3d000e2273aac258ba9b0e4
Logan Wright <logan.wright@valic.com>
2019-03-19 22:20:07 -0500

    fixes user model and updates snapshot

--- server/src/resources/user/__tests__/user.model.spec.js
+++ server/src/resources/user/__tests__/user.model.spec.js
@@ -2,34 +2,7 @@ import { User } from '../user.model';
 import mongoose from 'mongoose';
 
 describe('User model', () => {
-  describe('schema', () => {
-    test('email', () => {
-      const { email } = User.schema.obj;
-      expect(email).toEqual({
-        type: String,
-        required: true,
-        trim: true,
-        unique: true,
-        maxlength: 50
-      });
-    });
-
-    test('username', () => {
-      const { username } = User.schema.obj;
-      expect(username).toEqual({
-        type: String,
-        required: true,
-        unique: true,
-        trim: true
-      });
-    });
-
-    test('password', () => {
-      const { password } = User.schema.obj;
-      expect(password).toEqual({
-        type: String,
-        required: true
-      });
-    });
-  });
+  test('snapshot', () => {
+    expect(User.schema).toMatchSnapshot()
+  })
 });
