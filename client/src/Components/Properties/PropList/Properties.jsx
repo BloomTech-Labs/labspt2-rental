42dcee9fd688afc1dabf70383ce7def0f2539973
Jess Harrison <jessdev83@gmail.com>
2019-03-20 22:41:14 -0400

    Actions created
    made 'actionCreator' file to house actions.

--- client/src/Components/Properties/PropList/Properties.jsx
+++ client/src/Components/Properties/PropList/Properties.jsx
@@ -1,18 +1,37 @@
 import React, { Component } from "react";
-import DatePicker from "../../shared/DatePicker/DatePicker.jsx";
-import PropertyList from "./PropertyList";
-import { FlexRow, FlexColumn } from "../../../custom-components/index";
-import Search from "../../shared/Search/Search";
+import PropertyCard from "./PropertyCard";
+import { FlexColumn } from "custom-components";
 
 class Properties extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      properties: []
+    };
+  }
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      properties: nextProps.properties
+    });
+  }
+  cardHandleClick = id => {
+    this.props.history.push(`/dashboard/properties/${id}`);
+  };
   render() {
     return (
-      <FlexColumn>
-        <FlexRow>
-          <Search />
-          <DatePicker />
-        </FlexRow>
-        {/* <PropertyList /> */}
+      <FlexColumn width="800px" alignCenter style={{ position: "relative" }}>
+        {this.state.properties.map(property => {
+          return (
+            <PropertyCard
+              imageLoc={property.image}
+              header={property.name}
+              lineOneInfo={property.address}
+              lineTwoInfo={property.assistants}
+              lineThreeInfo={property.occupants}
+              buttonFunction={() => this.cardHandleClick(property._id)}
+            />
+          );
+        })}
       </FlexColumn>
     );
   }
