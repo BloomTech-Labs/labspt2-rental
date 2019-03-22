902816aed6110ca461e778e7ccefa96491961ef7
Michael Littleton <michaelglittleton@gmail.com>
2019-03-22 00:39:03 -0400

    Changes the employee API to utilize the user API instead

--- server/src/utils/BaseController.js
+++ server/src/utils/BaseController.js
@@ -38,11 +38,15 @@ export class BaseController {
     }
   }
 
-  getMany = async (req, res, next) => {
+  getMany = async (req, res, next, query) => {
     let filter,
       sort,
       skip,
       limit;
+
+    if (query) {
+      filter = {...filter, ...query}
+    }
 
     if (req.query.filter) {
       filter = JSON.parse(req.query.filter)
