735426dc5ea8a12a3919bfabeb8368b82f7e05f1
Bryce Evans <evans.bryce@gmail.com>
2019-03-27 22:00:37 -0600

    Revert "Add Redux to Tasks Page."

--- client/src/Components/Tasks/Tasks.js
+++ client/src/Components/Tasks/Tasks.js
@@ -2,30 +2,16 @@ import React, { Component } from 'react'
 import TaskList from './TaskList'
 import TaskPageTop from './TaskPageTop'
 
-class Tasks extends Component {
-  constructor(props) {
-    super(props);
-      this.state = {
-        tasks: [],
-        loading: false,
-        error: null
-      };
-    }
-    
-    componentDidMount() {
-      this.props.getTasks();
-    }
-
+export default class Tasks extends Component {
+  
   render () {
     return (
       <div>
         <TaskPageTop />
         <TaskList 
-          style={{ margin: "20px" }}
+          style={{margin: "20px"}}
         />
       </div>
     )
   }
 }
-
-export default Tasks;
--- client/src/Redux/rootReducer.js
+++ client/src/Redux/rootReducer.js
@@ -4,7 +4,6 @@ import registrationReducer from "../Components/Registration/reducer";
 import userListReducer from "../Components/UserList/reducer";
 import reservationsReducer from "../Components/Reservations/reducer";
 import employeesReducer from "../Components/Employees/reducers";
-import taskReducer from "../Components/Tasks/reducer";
 import loginReducer from "../Components/LoginPage/reducer";
 
 export default combineReducers({
@@ -13,6 +12,5 @@ export default combineReducers({
   auth: loginReducer,
   userList: userListReducer,
   reservations: reservationsReducer,
-  employees: employeesReducer,
-  tasks: taskReducer
+  employees: employeesReducer
 });
