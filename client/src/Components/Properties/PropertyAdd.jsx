c66f5a83dd07c9fea40240870c2c3e536642fa93
Jess Harrison <jessdev83@gmail.com>
2019-04-08 19:39:49 -0400

    Initial setup
    dashboard route added, container and index linked to component, component skeleton created, icon added to properties list for add option.

---
+++ client/src/Components/Properties/PropertyAdd.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class PropertyAdd extends Component {
+  render() {
+    return <h1>THIS IS THE ADD PROPERTY PAGE</h1>;
+  }
+}
+
+export default PropertyAdd;
--- client/src/Components/Properties/container.js
+++ client/src/Components/Properties/container.js
@@ -3,6 +3,7 @@ import { getProperties, getProperty, updateProperty } from "./actionCreator";
 import _Properties from "./Properties";
 import _Property from "./Property";
 import _PropertyEdit from "./PropertyEdit";
+import _PropertyAdd from "./PropertyAdd";
 import { withRouter } from "react-router-dom";
 
 const mapStateToProps = data => ({
@@ -20,3 +21,4 @@ const connector = connect(
 export const Properties = connector(withRouter(_Properties));
 export const Property = connector(withRouter(_Property));
 export const PropertyEdit = connector(withRouter(_PropertyEdit));
+export const PropertyAdd = connector(withRouter(_PropertyAdd));
