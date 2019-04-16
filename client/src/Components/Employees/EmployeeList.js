a9a237f308334716730b842dbde151a2467d44d0
Michael Littleton <40123075+mglittleton@users.noreply.github.com>
2019-04-15 22:53:02 -0400

    Changed the EmployeeList defaultActivePage back to unchanged

--- client/src/Components/Employees/EmployeeList.js
+++ client/src/Components/Employees/EmployeeList.js
@@ -19,7 +19,7 @@ const EmployeeList = props => {
           onPageChange={handlePageChange}
           className="space-bottom"
           boundaryRange={0}
-          defaultActivePage={1}
+          defaultActivePage={page}
           firstItem={null}
           lastItem={null}
           ellipsisItem={null}
