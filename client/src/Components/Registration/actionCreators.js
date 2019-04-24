81ddf9efe9c1c987e7107f57faa825c7d11d6c9d
Julie Jonak <juliejonak10@gmail.com>
2019-04-24 13:09:04 -0500

    Adding additional debug consoles

--- client/src/Components/Registration/actionCreators.js
+++ client/src/Components/Registration/actionCreators.js
@@ -6,6 +6,7 @@ export const registerUser = newUser => {
   return async dispatch => {
     dispatch({ type: actions.REGISTER_USER_STARTED });
     try {
+      console.log('hey we are trying to hit the end point')
       const token = await axios.post(
         `${config.apiUrl}/api/users/register`,
         newUser
@@ -14,7 +15,9 @@ export const registerUser = newUser => {
         type: actions.REGISTER_USER_SUCCESS,
         payload: "Bearer " + token.data.token
       });
+      console.log('we registered')
     } catch (err) {
+      console.log('uh oh', err.response.data.err)
       dispatch({
         type: actions.REGISTER_USER_FAILURE,
         payload: err.response.data.err.errmsg
