de64a2c2b57c95c830831e0cb6fd67100244907c
Julie Jonak <juliejonak10@gmail.com>
2019-05-02 14:11:01 -0700

    Adjusts single reservation view for improved mobile

--- client/src/Components/LandingPage/DesktopContainer.jsx
+++ client/src/Components/LandingPage/DesktopContainer.jsx
@@ -15,20 +15,11 @@ import { Footer } from "./Footer";
 import { GetStarted } from "./GetStarted";
 import { Features } from "./Features";
 
-// Heads up!
-// We using React Static to prerender our docs with server side rendering, this is a quite simple solution.
-// For more advanced usage please check Responsive docs under the "Usage" section.
-
 const getWidth = () => {
   const isSSR = typeof window === "undefined";
 
   return isSSR ? Responsive.onlyTablet.minWidth : window.innerWidth;
 };
-
-/* Heads up!
- * Neither Semantic UI nor Semantic UI React offer a responsive navbar, however, it can be implemented easily.
- * It can be more complicated, but you can create really flexible markup.
- */
 
 class DesktopContainer extends Component {
   state = {};
--- client/src/Components/LandingPage/LandingPage.jsx
+++ client/src/Components/LandingPage/LandingPage.jsx
@@ -3,11 +3,6 @@ import React from "react";
 import DesktopContainer from "./DesktopContainer";
 import MobileContainer from "./MobileContainer";
 
-// Current basic blue Semantic UI button color hex: #0080D6
-// Background light blue: #f6f9fc
-// Dark grey: #1a1b1c
-// Icon green: #4ca34b
-
 const ResponsiveContainer = ({ children }) => (
   <div>
     <DesktopContainer>{children}</DesktopContainer>
